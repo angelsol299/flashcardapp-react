a0a902a07388e521dd7f1a1ca5179bab517c3fcc
angelsol299 <angelsol299@hotmail.com>
2018-12-01 14:25:32 +0100

    card structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import FlasCards from "./components/flashCards";
+import Card from "./Card/Card";
 
 import "./App.css";
 
@@ -7,8 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome</h1>
-        <FlasCards />
+        <Card />
       </div>
     );
   }
