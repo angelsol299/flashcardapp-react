8fca37ccd0e5d015429502e77b5a2ec5c6374451
angelsol299 <angelsol299@hotmail.com>
2018-12-01 16:52:21 +0100

    draw card and draw button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React, { Component } from "react";
 import Card from "./Card/Card";
+import DrawButton from "./DrawButton/DrawButton";
 
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.updateCard = this.updateCard.bind(this);
+
+    this.state = {
+      cards: [
+        { id: 1, eng: "English", han: "Hnazi", pin: "Pinyin" },
+        { id: 2, eng: "English_2", han: "Hnazi_2", pin: "Pinyin_2" }
+      ],
+      currentCard: {}
+    };
+  }
+
+  componentWillMount() {
+    const currentCards = this.state.cards;
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    });
+  }
+
+  getRandomCard(currentCards) {
+    const card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    return card;
+  }
+
+  updateCard() {
+    console.log("working method");
+  }
+
   render() {
     return (
       <div className="App">
-        <Card />
+        <div className="cardRow">
+          <Card
+            eng={this.state.currentCard.eng}
+            han={this.state.currentCard.han}
+            pin={this.state.currentCard.pin}
+          />
+          <div className="buttonRow">
+            <DrawButton drawCard={this.updateCard} />
+          </div>
+        </div>
       </div>
     );
   }
