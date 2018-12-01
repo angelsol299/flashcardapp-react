c7d0e3af5c7ee57afeaaf0968278570b57f575a7
angelsol299 <angelsol299@hotmail.com>
2018-12-01 21:41:57 +0100

    card css styling added

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyACbml5SG2Gb0YRb4W5PGunqmPM9c7FEHE",
+  authDomain: "recipe-beabf.firebaseapp.com",
+  databaseURL: "https://recipe-beabf.firebaseio.com",
+  projectId: "recipe-beabf",
+  storageBucket: "recipe-beabf.appspot.com",
+  messagingSenderId: "771709045613"
+};
---
+++ src/components/flashCards.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class FlashCards extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome</h2>
+      </div>
+    );
+  }
+}
+
+export default FlashCards;
